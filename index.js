516df697b94d18b876e04f6ce1ac06731d4f4db6
Justin T. Dejesus <rr605855@fincoad.com>
2018-09-19 12:09:44 -0500

    deal with array of defined objects

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { graphql, buildSchema } = require ('graphql');
 const schema = buildSchema(`
 type Query {
   video: Video
+  videos: [Video]
 }
 
 type Video {
@@ -20,20 +21,35 @@ type Schema {
 }
 `);
 
+const videoA = {
+  id: 'a', 
+  title: 'Video A',
+  duration: 120,
+  watched: true,
+};
+const videoB = {
+  id: 'b', 
+  title: 'Video B',
+  duration: 240, 
+  watched: false,
+};
+const videos = [videoA, videoB];
+
 const resolvers = {
   video: () => ({
     id: () => '1',
     title: () => 'bar',
     duration: () => 180,
     watched: () => true
-  })
+  }),
+  videos: () => videos
 
 
 };
 
 const query = `
 query myFirstQuery {
-  video {
+  videos {
     id,
     title,
     duration,
